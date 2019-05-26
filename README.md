17009d633f5bebcb6c8cdba4f872c12bf3d41fdd
alexandre_lavenant <alavenant@ccmbenchmark.com>
2019-05-26 20:21:34 +0200

    init fastswap

---
+++ README.md
@@ -0,0 +1,24 @@
+README
+
+Simple cli to get your tickets automatically from TicketSwap website.
+
+I. Get the repository
+--------------------
+```
+git clone https://github.com/alexandreLavenant/fastswap.git
+```
+II. Installation
+---------------
+```
+yarn install
+```
+or
+```
+npm install
+```
+IV. Run Program
+---------------
+See the help
+```
+node fastswap --help
+```
---
+++ fastswap.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const puppeteer	= require('puppeteer'),
+	program		= require('commander'),
+	$			= require('cheerio'),
+	baseUrl		= 'https://www.ticketswap.fr',
+	bookTicket = async function(browser, swapUrl)
+	{
+		if (pageCommon === null) {
+			pageCommon = await browser.newPage();
+		}
+
+		const response = await pageCommon.goto(swapUrl),
+			status = response.status(),
+			body = await response.text(),
+			availableTicket = parseInt($(body).find('h2.css-1wu73kq.e149jiyc1').first().text())
+            ;
+            
+        if (availableTicket === 0) {
+			await sleep(10e3); // wait 10s
+			await bookTicket(browser, swapUrl);
+        }
+
+        const ticketNodes = $(body).find('a.css-1qka4wa.e15p5mol1');
+        ticketNodes.each(async (i, ticketNode) => {
+			var ticketUrl = $(ticketNode).attr('href'),
+				page = await browser.newPage()
+				;
+
+			page.goto(baseUrl + ticketUrl);
+		});
+	},
+	sleep = function(ms)
+	{
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+	;
+	
+program
+	.usage('-u <url ...>')
+	.version(require('./package.json').version, '-v, --version')
+	.option('-u, --url <url>', 'Ticket swap url of the event')
+	.parse(process.argv)
+	;
+
+var pageCommon = null;
+
+puppeteer.launch({ headless: false })
+.then(async browser =>
+{
+    await bookTicket(browser, program.url);
+});
